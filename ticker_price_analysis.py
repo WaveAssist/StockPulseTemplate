c436da9079ce64f85faef686f44bf807c7986a4d
WaveAssist <kakshil.shah@waveassist.io>
2025-06-13 15:31:16 +0530

    Update ticker_price_analysis.py

--- ticker_price_analysis.py
+++ ticker_price_analysis.py
@@ -2,32 +2,35 @@ import waveassist
 waveassist.init()
 import pandas as pd
 
-ticker_data = waveassist.fetch_data("ticker_data")
+tickers_str = waveassist.fetch_data("tickers")
+tickers = [t.strip() for t in tickers_str.split(",")]
+
+ticker_data = waveassist.fetch_data("ticker_data") 
 
 df_list = []
-for ticker, data in ticker_data.items():
-    info = data['info']
-    hist = data['hist']
-    
+for ticker in tickers:
+    data = ticker_data.get(ticker, {})
+    info = data.get("info", {})
+
     current_price = info.get('currentPrice') or info.get('regularMarketPrice')
     previous_close = info.get('previousClose') or info.get('regularMarketPreviousClose')
-    
+
     if current_price and previous_close:
         day_change_pct = ((current_price - previous_close) / previous_close) * 100
     else:
         day_change_pct = 0
-    
+
     premarket_price = info.get('preMarketPrice')
     premarket_change_pct = 0
     if premarket_price and previous_close:
         premarket_change_pct = ((premarket_price - previous_close) / previous_close) * 100
-    
+
     df_list.append({
         'Ticker': ticker,
         'Current Price': current_price or 0,
-        'Day Change %': day_change_pct,
-        'Pre-Market Change %': premarket_change_pct
+        'Day Change %': round(day_change_pct, 2),
+        'Pre-Market Change %': round(premarket_change_pct, 2)
     })
 
 df = pd.DataFrame(df_list)
-waveassist.store_data("price_dataframe", df)
+waveassist.store_data("stock_dataframe", df)
